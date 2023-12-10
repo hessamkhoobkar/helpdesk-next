3f27449784b7b86212b8e30b618a840cba32fb98
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-12-10 16:42:46 +0330

    update app layout to support card design

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-zinc-100 dark:bg-zinc-950 text-zinc-950 dark:text-white">
       <AppNavbar />
-      {children}
+
+      <main className="w-full max-w-5xl mx-auto min-h-screen px-6 py-4">
+        <div className="bg-white dark:bg-zinc-900 p-4 rounded-xl">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
