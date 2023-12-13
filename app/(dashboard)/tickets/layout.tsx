96e23fb4c8a2f7bff34761e67572e6092163b31c
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-12-13 18:13:11 +0330

    Refactor ticket layout component

--- app/(dashboard)/tickets/layout.tsx
+++ app/(dashboard)/tickets/layout.tsx
@@ -1,6 +1,5 @@
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <div className="bg-white dark:bg-zinc-900 p-4 rounded-xl"></div>
-    <>{children}</>
+    <div className="bg-white dark:bg-zinc-900 p-4 rounded-xl">{children}</div>
   );
 }
