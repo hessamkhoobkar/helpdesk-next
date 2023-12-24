58fb393ab16b89488b1c7a625c81f5ea8eb22aef
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-12-24 18:32:24 +0330

    Update Navigation spacing

--- app/(dashboard)/_components/navbar/Navigation.tsx
+++ app/(dashboard)/_components/navbar/Navigation.tsx
@@ -22,11 +22,11 @@ const menuItems = [
 export default function Navigation() {
   const pathname = usePathname();
   return (
-    <NavbarContent className="hidden sm:flex gap-4 ms-8" justify="center">
+    <NavbarContent className="hidden sm:flex gap-1 ms-8" justify="center">
       {menuItems.map((item) => (
         <NavbarItem key={item.label}>
           <Link
-            className={`font-medium py-4 px-2 rounded-xl hover:text-white dark:hover:text-white transition-all duration-200 ease-in-out ${
+            className={`font-medium p-4 rounded-xl hover:text-white dark:hover:text-white transition-all duration-200 ease-in-out ${
               pathname === item.href
                 ? "text-white"
                 : "text-primary-200 dark:text-primary-700"
