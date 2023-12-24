7ff15e2fe6d179605370ad61cb003d8f42a4c57f
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-12-24 16:25:45 +0330

    Remove console.log statement in categories route

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const categories = await prisma.category.findMany();
-  console.log("on server", categories);
 
   return NextResponse.json(categories, { status: 200 });
 }
