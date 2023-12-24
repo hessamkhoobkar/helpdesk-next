42f7bef369a4552f7c77353a7dc8a4951cfa5656
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-12-24 18:28:19 +0330

    Update theme colors in layout and tailwind.config

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,7 +2,7 @@ import AppNavbar from "./_components/Navbar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-zinc-100 dark:bg-zinc-950 text-zinc-950 dark:text-white">
+    <div className="bg-content2 text-foreground dark:bg-zinc-950  dark:text-white">
       <AppNavbar />
       <main className="w-full max-w-7xl mx-auto min-h-screen px-6 py-4">
         {children}
