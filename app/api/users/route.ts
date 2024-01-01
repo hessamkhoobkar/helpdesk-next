8d9c81459016c508ff0348a1d5d5fe07d1affecd
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-01 17:34:40 +0330

    Remove console.log statements in users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,15 +5,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
   const params = new URLSearchParams(request.nextUrl.searchParams);
   const userTypeQuery = params.get("userType") || undefined;
-  console.log(userTypeQuery);
 
   const users = await prisma.user.findMany({
     where: {
       type: userTypeQuery as UserType,
     },
   });
-
-  console.log(users);
 
   if (!users) {
     return NextResponse.json({ error: "No users found" }, { status: 404 });
