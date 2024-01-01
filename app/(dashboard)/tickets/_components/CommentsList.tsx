659f8355cdce388a2454e0e07d6e6aa900b5b3a1
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-01 04:00:05 +0330

    Update CommentsList component layout and add work in progress message

--- app/(dashboard)/tickets/_components/CommentsList.tsx
+++ app/(dashboard)/tickets/_components/CommentsList.tsx
@@ -18,13 +18,23 @@ export default function CommentsList({
   comments?: Comment[];
 }) {
   return (
-    <div className="grid grid-cols-12 gap-4 mt-8">
-      <div className="col-span-7">
+    <div className="grid grid-cols-12 gap-8 mt-8">
+      <div className="col-span-11 md:col-span-7">
         <CommentCard
           user={firstComment.user}
           date={firstComment.date}
           comment={firstComment.comment}
         />
+      </div>
+      <div className="col-span-12 md:col-span-7 md:col-start-6 bg-default-200 dark:bg-default-100 rounded-2xl p-6 md:p-8">
+        <h2 className="text-xl font-bold mb-2 text-primary">
+          Work in progress
+        </h2>
+        <p>
+          Comments implications are coming soon. Clients, employees, and admins
+          can comment on a ticket and give updates on its progress. Comments can
+          not be deleted or edited to keep a record of them.
+        </p>
       </div>
     </div>
   );
