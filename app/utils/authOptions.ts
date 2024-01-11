c6fbe321063228d86ec7482eea318fed9eb664fb
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-12 00:09:20 +0330

    Refactor authentication options

---
+++ app/utils/authOptions.ts
@@ -0,0 +1,79 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/prisma/client";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Sign in",
+      id: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !(credentials.password === user.password!)) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          image: user.avatar,
+          name: user.user_name,
+          userType: user.type,
+          randomKey: "flyoverthemoon",
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    session: ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          userType: token.userType,
+          randomKey: token.randomKey,
+        },
+      };
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+
+        return {
+          ...token,
+          id: u.id,
+          userType: u.userType,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
